248af6dabd063157c868dd9bef767fab9c6d8cbe
kubraakmansoy <fndkbrpln@gmail.com>
2025-07-22 23:40:14 +0300

    Add empty UserController to WebAPI project

---
+++ App.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
