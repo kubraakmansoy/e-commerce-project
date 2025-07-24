695c5b42d1f0853457d492e8dc400516e39b2c81
kubraakmansoy <fndkbrpln@gmail.com>
2025-07-24 14:33:59 +0300

    Add JwtSettings class and configure JWT settings structure

--- App.Core/Settings/JwtSettings.cs
+++ App.Core/Settings/JwtSettings.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace App.Core.Settings
+namespace App.WebAPI.Settings
 {
-    internal class JwtSettings
+    public class JwtSettings
     {
+        public string SecretKey { get; set; } = null!;
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public int ExpirationMinutes { get; set; }
     }
 }
+
+
+
