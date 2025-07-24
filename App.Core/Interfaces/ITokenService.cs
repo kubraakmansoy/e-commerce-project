fcb956f3f80c7480325507f1c11db2565d7160bb
kubraakmansoy <fndkbrpln@gmail.com>
2025-07-24 15:59:58 +0300

    Add TokenService class to App.Service project

---
+++ App.Core/Interfaces/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Core.DTOs;
+
+
+namespace App.Core.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(UserDto user);
+    }
+}
+
